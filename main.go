7b4f1301d3422cd15e5c36930248166bdc77b66a
ninjinx <responsura@gmail.com>
2023-10-23 01:18:36 +0900

    test

--- main.go
+++ main.go
@@ -457,7 +457,7 @@ func (h *Handler) obtainPresent(tx *sqlx.Tx, userID int64, requestAt int64) ([]*
 	query = "SELECT * FROM user_present_all_received_history WHERE user_id=? AND present_all_id in (" + strings.Join(npIds, ",") + ")"
 	err := tx.Select(receivedArray, query, userID)
 	if err != sql.ErrNoRows {
-		return nil, errors.New(query)
+		return nil, err
 	}
 
 	receivedStatus := make(map[int64]bool, len(receivedArray))
